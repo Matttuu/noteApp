4700f1827c5e70327ab79903ea49215efb6f6528
Mattu <xuniqh@gmail.com>
2018-10-04 22:33:32 +0200

    Added mongoDB Connection | Made server availability rely on db connection

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); 
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
+
+var db;
+
+MongoClient.connect('mongodb://<admin>:<Admin123>@ds223253.mlab.com:23253/note-app', (err, database) => {
+    if(err) return console.log(err)
+        db = client.db('note-app') 
+        app.listen(3000, () => {
+            console.log('Listening on 3000');
+        })
+})
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -12,7 +23,4 @@ app.post('/quotes', (req, res) => {
     console.log(req.body);
 })
 
-app.listen(3000, function() {
-    console.log('Listening on 3000');
-})
 console.log('Hello world!');
