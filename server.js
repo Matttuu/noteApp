63596cee76c66d87f6aa230553d8242f1fe98586
Mattu <xuniqh@gmail.com>
2018-10-05 00:07:44 +0200

    Fixed database error.

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
-var db;
 
-MongoClient.connect('mongodb://<admin>:<Admin123>@ds223253.mlab.com:23253/note-app', (err, database) => {
-    if(err) return console.log(err);
-        db = client.db('note-app'); 
+var db
+
+MongoClient.connect('mongodb://admin:Admin123@ds223253.mlab.com:23253/note-app', {useNewUrlParser: true},  (err, client) => {
+    
+    
+    if(err) return console.log(err)
+    
+        db = client.db('note-app')
         app.listen(3000, () => {
-            console.log('Listening on 3000');
+            
+            console.log('Listening on 3000')
         })
 })
 
@@ -19,12 +24,13 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
+
 app.post('/quotes', (req, res) => {
     db.collection('quotes').save(req.body, (err, result) => {
         if (err) return console.log(err);
 
         console.log('saved to database');
-        res.redirect('/');
+        res.redirect('/'); // Returner brugeren tilbage til / for at undgå konstant loading-phase.
     })
 })
 
