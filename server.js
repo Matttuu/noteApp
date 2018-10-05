eb0517294450928aa20fcbc373e2ed93d3171103
Mattu <xuniqh@gmail.com>
2018-10-06 00:39:15 +0200

    Fixed DB bug + Cleaned up code. App Create and Get not works perfectly.

--- server.js
+++ server.js
@@ -1,45 +1,31 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser'); 
 const MongoClient = require('mongodb').MongoClient;
-const app = express();
-
 
 var db
 
 MongoClient.connect('mongodb://admin:Admin123@ds223253.mlab.com:23253/note-app', {useNewUrlParser: true},  (err, client) => {
-    
-    
     if(err) return console.log(err)
-    
         db = client.db('note-app')
         app.listen(3000, () => {
-            
             console.log('Listening on 3000')
         })
 })
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-//app.get('/', (req, res) => {
-    //res.sendFile(__dirname + '/index.html');
-//})
-
 app.get('/', (req, res) => {
-    var cursor = db.collection('quotes').find();
-    db.collection('quotes').find().toArray(function(err, results) {
-        console.log(results)   
-      })
-    
-})
-
-app.set('view engine', 'ejs');
-
-res.render(view, locals);
+    db.collection('quotes').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      res.render('index.ejs', {quotes: result})
+    })
+  })
 
 app.post('/quotes', (req, res) => {
     db.collection('quotes').insertOne(req.body, (err, result) => {   // Opretter collection 'quotes' i db hvis den ikke eksiterer, hvis den eksiterer så tilføjer den til collectionen.
         if (err) return console.log(err);
-
         console.log('saved to database');
         res.redirect('/'); // Returner brugeren tilbage til / for at undgå konstant loading-phase.
     })
