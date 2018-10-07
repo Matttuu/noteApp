27b0e1b1aa9424a04132051a03b7807ad01eda60
Mattu <xuniqh@gmail.com>
2018-10-07 20:50:11 +0200

    Populated main.js, tilføjet middleware til at læse JSON data.

--- public/main.js
+++ public/main.js
@@ -1,1 +1,14 @@
-app.use(express.static('public'))
+var update = document.getElementById('update')
+
+update.addEventListener('click', function () {
+
+    fetch('quotes', {
+        method: 'put',  // PUT request
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify({  // Converter til JSON format
+          'name': 'Darth Vader',
+          'quote': 'I find your lack of faith disturbing.'
+        })
+      })
+    
+  })
--- server.js
+++ server.js
@@ -9,12 +9,15 @@ MongoClient.connect('mongodb://admin:Admin123@ds223253.mlab.com:23253/note-app',
     if(err) return console.log(err)
         db = client.db('note-app')
         app.listen(3000, () => {
-            console.log('Listening on 3000')
+            console.log('Listening on 3000');
         })
 })
 
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public')); // Fortæller express at demme folder er tilgængelig for offentligen, ved brug af built-in middleware "express.static".
+app.use(bodyParser.json());  // Ved at benytte os af denne middleware kan serveren nu læse JSON data.
 
 app.get('/', (req, res) => {
     db.collection('quotes').find().toArray((err, result) => {
@@ -30,5 +33,9 @@ app.post('/quotes', (req, res) => {
         res.redirect('/'); // Returner brugeren tilbage til / for at undgå konstant loading-phase.
     })
 })
+
+app.put('/quotes', (req, res) => {
+    
+  })
 
 console.log('Hello world!');
